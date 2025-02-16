b5e19ad5a626e9399e4911904919cb811bcdcb19
AL <153260270+cikalkautsar@users.noreply.github.com>
2025-02-16 21:54:16 +0700

    Create nomor2.java

---
+++ Tugas Pertemuan 2/nomor2.java
@@ -0,0 +1,27 @@
+package com.cikalstudio.tugas2;
+
+/*2) Buatlah algoritma dalam Bahasa Java Anda akan keluar membeli migacoan jika  mempunyai uang lebih besar dari 10000 
+(rupiah) dan pasangan Anda sedang tidak ad a  kelas di kuliahanya, yang dapat ditulis sebagai 
+ekspresi: (uang > 10000) &&  (!tidakAdaKelasKuliah) 
+ */
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Tugas2b {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Berapa emang uang kamu? ");
+        int uang = input.nextInt();
+
+        System.out.print("Pacar km emg gada kelas? (true kalau gak ada, false kalau ada): ");
+        boolean tidakAdaKelasKuliah = input.nextBoolean();
+
+        boolean bisaBeliMigacoan = (uang > 10000) && tidakAdaKelasKuliah;
+
+        System.out.println("Jadi bisa ga nih? (true kalau bisa, false kalau gabisa) " + bisaBeliMigacoan);
+
+        input.close();
+    }
+}
