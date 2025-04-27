0dc31b288fb5fd80a190306ce417bf99c453666a
AL <153260270+cikalkautsar@users.noreply.github.com>
2025-04-27 23:57:14 +0700

    Create person.java

---
+++ Cikal Kautsar 2306115/Tugas pertemuan 8-9/no2/person.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cikalstudio.tugas8_9;
+
+// Kelas Person
+public class Person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public Person(String name, String address, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Overridden toString method
+    @Override
+    public String toString() {
+        return "Person: " + name;
+    }
+}
+
